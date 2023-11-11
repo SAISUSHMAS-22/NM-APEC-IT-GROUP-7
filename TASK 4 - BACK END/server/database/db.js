bf9afc9cd1a07e143f6f5c08251f91562c77d9c3
SAISUSHMAS-22 <sushmasuresh238@gmail.com>
2023-11-11 18:26:07 +0530

    Making my first commit

---
+++ TASK 4 - BACK END/server/database/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+
+const Connection = async (username, password) => {
+    const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@clone-chatty.50baicb.mongodb.net/`;
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true});
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+
+};
+
+export default Connection;
